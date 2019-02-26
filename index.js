dd11553eb11110718170f7b874e64db69640d3ae
Jeremy McWilliams <jeremym@lclark.edu>
2019-02-26 09:16:35 -0800

    some commits!

---
+++ index.js
@@ -0,0 +1,2 @@
+require('./js/my-demo.module.js')   //replace module-name with a real name
+module.exports = 'my-demo'    //replace my-module with the real module name
---
+++ js/my-demo.module.js
@@ -0,0 +1,11 @@
+angular
+  .module('my-demo', [])    //this should match the value for module.exports in index.js
+  .component('prmActionContainerAfter', {
+  bindings: { parentCtrl: '<' },
+    controller: function controller($scope) {
+        this.$onInit = function() {
+          console.log("my demo!");
+        };
+    },
+  template: '<div>PCSG DEMO mor stuff!</div>'
+});
